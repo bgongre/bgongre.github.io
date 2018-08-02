8edfb09da45ee4dd9f23ad95170c0bbb0d263d50
bgongre <27456325+bgongre@users.noreply.github.com>
2018-08-02 10:16:08 -0400

    updated projects and resume link

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function activeNav(e) {
 }
 
 //Scroll back to top button
-$(document).ready(function(){
+$(document).ready(() => {
 
     //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
+    $(window).scroll(() => {
         if ($(this).scrollTop() > 100) {
             $('.scrollToTop').fadeIn();
         } else {
@@ -35,22 +35,27 @@ $(document).ready(function(){
     });
 });
 
-//Smooth scrolling anchors
-$(document).ready(function(){
-  //Add smooth scrolling to anchors
+//Scrolling anchors
+$(document).ready(function() {
+  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
+    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
+      // Prevent default anchor click behavior
       event.preventDefault();
 
+      // Store hash
       var hash = this.hash;
 
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
 
+        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } 
+    } // End if
   });
 });
